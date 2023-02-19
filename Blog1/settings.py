b2f3b84293d4a3d58afe3e164b207b77edc53813
Francisco Lucena <franciscolucena90@gmail.com>
2023-02-19 19:50:52 -0300

    updating database

--- Blog1/settings.py
+++ Blog1/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'Blog1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'iotd',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
