3bedab1cd2e5d5dfd5ca36bd721a9b5474f14166
Francisco Lucena <franciscolucena90@gmail.com>
2023-02-19 19:46:01 -0300

    debugging database

--- Blog1/settings.py
+++ Blog1/settings.py
@@ -81,6 +81,8 @@ WSGI_APPLICATION = 'Blog1.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
