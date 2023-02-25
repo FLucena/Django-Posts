89936953925e9ae55adcbafb85f7ca1b6cfefe1b
Francisco Lucena <franciscolucena90@gmail.com>
2023-02-25 00:03:07 -0300

    updates v2

--- Blog1/settings.py
+++ Blog1/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('Key')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app',
+                 '127.0.0.1']
 
 
 # Application definition
@@ -86,7 +87,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'Luche1990.',
         'HOST': 'localhost',
-        'PORT': '8000',
+        'PORT': '5433',
     }
 }
 
